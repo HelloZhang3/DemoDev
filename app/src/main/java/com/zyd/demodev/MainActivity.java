49c971fdfc21225fabdcd60eda12809a1f704195
zhnagyadong <yadong9416@126.com>
2018-08-22 17:31:47 +0800

    commit

---
+++ app/src/main/java/com/zyd/demodev/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zyd.demodev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author zhangyadong
+ * @class main activity
+ * @time 2018/7/25 10:31
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/zyd/demodev/MainActivityKotlin.kt
+++ app/src/main/java/com/zyd/demodev/MainActivityKotlin.kt
@@ -3,7 +3,7 @@ package com.zyd.demodev
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivityKotlin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
